acad64b1b39fcb938f19c7afd9ebeb8e2efe016d
Md Rizwan Ansari <85256728+mdrizwan7739@users.noreply.github.com>
2023-01-13 20:01:22 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -4,18 +4,11 @@ const handleClick = () => {
   let b = document.getElementById('select').value;
   let a = document.createElement('div');
   a.className = b;
-  
-  a.innerHTML = -1;
-  a.innerHTML =parseInt(a.innerHTML)+1;
   document.getElementById('shapes-holder').appendChild(a);
 }
 const App = () => {
   return (
-
     <div id="main">
-  
-
-
       <div id="shape-creator">
         <select id='select' >
           <option value="Square">Square</option>
