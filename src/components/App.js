1745a355dac65f0520c986ae3cfead89cfadbaa5
Md Rizwan Ansari <85256728+mdrizwan7739@users.noreply.github.com>
2023-01-13 19:36:32 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,25 @@
 import React from 'react'
 import '../styles/App.css';
-const App = () => {
+const handleClick = () => {
+  let b = document.getElementById('select').value;
 
+  let a = document.createElement('div');
+  a.className = b;
+  document.getElementById('shapes-holder').appendChild(a);
+}
+const App = () => {
   return (
+
     <div id="main">
+
+
+
       <div id="shape-creator">
-        
+        <select id='select' >
+          <option value="square">square</option>
+          <option value="circle">circle</option>
+        </select>
+        <button onClick={handleClick}>Add Shape</button>
 
       </div>
       <div id="shapes-holder">
